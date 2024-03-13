3379ea854040ec8740ac35fd4a726af4cef1592c
Vanessa Chan <chw.vanessa@gmail.com>
2024-03-12 23:57:11 -0400

    MAJ de la grille de projets

--- vanportfolio/src/app/Components/Projects/Projects.js
+++ vanportfolio/src/app/Components/Projects/Projects.js
@@ -9,17 +9,112 @@ const Projects = () => {
   return (
     <Container id="section-projets">
       <Content>
-        <h1>Projets</h1>
+        <h1>&gt; mes projets</h1>
         <p>
-          Liste de projets réalisés en développement web, soit dans le cadre
-          d'une formation, soit pour un projet personnel.{" "}
+          Exemples de projets réalisés en développement web dans le cadre d'une
+          formation ou pour un projet personnel. Voir{" "}
+          <LienGithub href="https://github.com/VanessaCHW" target="_blank">
+            github
+          </LienGithub>{" "}
+          pour la liste complète des projets.
         </p>
 
         <ProjectList>
-          {ProjectsData.map((project) => (
-            <ProjectBox project={project} key={"project_" + project.id} />
-          ))}
+          <ProjBox>
+            <ImgContainer>
+              <FolioImg src="portfoliobrowser.png" />
+            </ImgContainer>
+            <ProjLinks>
+              PROJET PERSO / 2024 /{" "}
+              <CodeLink
+                href="https://github.com/VanessaCHW/vc-portfolio"
+                target="_blank"
+              >
+                GITHUB
+              </CodeLink>{" "}
+              /{" "}
+              <CodeLink href="https://vanportfolio.vercel.app/" target="_blank">
+                DÉMO
+              </CodeLink>
+            </ProjLinks>
+            <ProjTitle>Site personnel / portfolio</ProjTitle>
+            <div>
+              Page personnelle crée avec Next. Page personnelle crée avec Next.
+              Page personnelle crée avec Next. Page personnelle crée avec Next.{" "}
+            </div>
+          </ProjBox>
+          <ProjBox>
+            <ImgContainer>
+              <PinkPlanImg src="pinkplanphones.png" />
+            </ImgContainer>
+            <ProjLinks>
+              CONCORDIA BOOTCAMP / 2021 /{" "}
+              <CodeLink
+                href="https://github.com/VanessaCHW/pink-planner"
+                target="_blank"
+              >
+                GITHUB & DÉMO(.GIF)
+              </CodeLink>
+            </ProjLinks>
+            <ProjTitle>"Pink Planner" - Application de calendrier</ProjTitle>
+            <div>
+              Page personnelle crée avec Next. Page personnelle crée avec Next.
+              Page personnelle crée avec Next. Page personnelle crée avec Next.{" "}
+            </div>
+          </ProjBox>
         </ProjectList>
+
+        <ProjectList>
+          <ProjBox>
+            <ImgContainer>
+              <FolioImg src="snow.png" />
+            </ImgContainer>
+            <ProjLinks>
+              CONCORDIA BOOTCAMP / 2021 /{" "}
+              <CodeLink
+                href="https://github.com/VanessaCHW/vc-portfolio"
+                target="_blank"
+              >
+                GITHUB
+              </CodeLink>{" "}
+              /{" "}
+              <CodeLink href="https://vanportfolio.vercel.app/" target="_blank">
+                DÉMO
+              </CodeLink>
+            </ProjLinks>
+            <ProjTitle>"Snowball escape" - Jeu Javascript</ProjTitle>
+            <div>
+              Page personnelle crée avec Next. Page personnelle crée avec Next.
+              Page personnelle crée avec Next.{" "}
+            </div>
+          </ProjBox>
+          <ProjBox>
+            <ImgContainer>
+              <FolioImg src="twit1.png" />
+            </ImgContainer>
+            <ProjLinks>
+              CONCORDIA BOOTCAMP / 2021 /{" "}
+              <CodeLink
+                href="https://github.com/VanessaCHW/vc-portfolio"
+                target="_blank"
+              >
+                GITHUB
+              </CodeLink>{" "}
+            </ProjLinks>
+            <ProjTitle>Twitter clone</ProjTitle>
+            <div>
+              Page personnelle crée avec Next. Page personnelle crée avec Next.
+              Page personnelle crée avec Next.{" "}
+            </div>
+          </ProjBox>
+        </ProjectList>
+
+        <Subtitle>&gt; mises à jour au portfolio à venir </Subtitle>
+        <ul>
+          <li>Design pour format mobile / tablette</li>
+          <li>Page individuelle pour chaque projet</li>
+          <li>Dark theme</li>
+        </ul>
       </Content>
     </Container>
   );
@@ -30,7 +125,7 @@ const Container = styled.div`
   margin-bottom: 100px;
   display: flex;
   justify-content: center;
-  background-color: #dde9ed; /** Bleu-gris pale */
+  /*background-color: #dde9ed;*/ /** Bleu-gris pale */
 `;
 
 const Content = styled.div`
@@ -40,7 +135,7 @@ const Content = styled.div`
   margin-bottom: 100px;
 `;
 
-const ProjectList = styled.div`
+/*const ProjectList = styled.div`
   width: 100%;
   display: flex;
   gap: 30px;
@@ -48,6 +143,68 @@ const ProjectList = styled.div`
   flex-wrap: wrap;
   justify-content: center;
   margin-top: 50px;
+`;*/
+
+const LienGithub = styled.a`
+  color: var(--global-pink);
+  text-decoration: underline;
+  font-weight: 500;
+`;
+
+const ProjectList = styled.div`
+  display: flex;
+  gap: 30px;
+  margin-top: 50px;
+`;
+
+const ProjBox = styled.div`
+  flex-basis: 50%;
+  flex-grow: 1;
+`;
+
+const ImgContainer = styled.div`
+  background-color: #ededed;
+  height: 420px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  overflow: hidden;
+`;
+
+const FolioImg = styled.img`
+  max-width: 90%;
+`;
+
+const PinkPlanImg = styled.img`
+  max-width: 82%;
+  position: absolute;
+  top: 50px;
+`;
+
+const ProjLinks = styled.div`
+  color: #929292;
+  font-family: "Quicksand", sans-serif;
+  font-weight: 600;
+  margin-top: 18px;
+  font-size: 16px;
+`;
+
+const ProjTitle = styled.div`
+  font-weight: 600;
+  margin-top: 6px;
+  margin-bottom: 3px;
+  font-size: 18px;
+`;
+
+const CodeLink = styled.a`
+  color: var(--global-pink);
+  text-decoration: underline;
+`;
+
+const Subtitle = styled.h2`
+  margin-top: 50px;
+  margin-bottom: 20px;
 `;
 
 export default Projects;
--- vanportfolio/src/app/GlobalStyle.js
+++ vanportfolio/src/app/GlobalStyle.js
@@ -4,7 +4,8 @@ const GlobalStyle = createGlobalStyle`
 
 :root{
     /**Accessible everywhere */
-    --max-width: 1100px;
+    --max-width: 1100px;/*1100px;*/
+    --max-content-width:1100px;
     --border-radius: 12px;
     --font-mono: ui-monospace, Menlo, Monaco, "Cascadia Mono", "Segoe UI Mono",
     "Roboto Mono", "Oxygen Mono", "Ubuntu Monospace", "Source Code Pro",
@@ -41,7 +42,7 @@ html, body {
 
     font-family: "Poppins", sans-serif;
     font-weight: var(--regular-font-weight);
-    font-size: var(--regular-font-size);
+    font-size: var(--second-font-size);
     /*line-height: 1.5;*/
     
 }
@@ -54,11 +55,12 @@ a {
 h1,h2,h3,h4{
   letter-spacing: 1px;
   color:var(--theme-black);
+  font-weight: 400;
 }
 
 h1 {
   font-size: 40px;
-  font-weight: 700;
+  font-weight: 400;
   margin-bottom: 30px;
 }
 
